7a32bd567cdbcfddd08923a7b86f337d9663c6a6
Karen Ly <lykaren27@gmail.com>
2020-10-02 23:51:06 -0700

    Completed Part 2

---
+++ flixster/MoveGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MoveGridCell.swift
+//  flixster
+//
+//  Created by Karen Ly on 10/2/20.
+//  Copyright © 2020 lykaren27@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MoveGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Karen Ly on 10/2/20.
+//  Copyright © 2020 lykaren27@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
